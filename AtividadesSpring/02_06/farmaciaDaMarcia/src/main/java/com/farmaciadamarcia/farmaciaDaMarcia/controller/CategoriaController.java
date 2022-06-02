6b6c631151f837257cd0da417fa11785432f5d86
João Lucas Marques Bento <joaolmb2@gmail.com>
2022-06-02 14:20:35 -0300

    Adicionando PUT que ficou faltando

--- AtividadesSpring/02_06/farmaciaDaMarcia/src/main/java/com/farmaciadamarcia/farmaciaDaMarcia/controller/CategoriaController.java
+++ AtividadesSpring/02_06/farmaciaDaMarcia/src/main/java/com/farmaciadamarcia/farmaciaDaMarcia/controller/CategoriaController.java
@@ -2,6 +2,8 @@ package com.farmaciadamarcia.farmaciaDaMarcia.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,9 +43,14 @@ public class CategoriaController {
 		return ResponseEntity.ok(repository.findAllByDescricaoCategoriaContainingIgnoreCase(nome));
 	}
 	
-	@PostMapping
-	public ResponseEntity<Categoria> AddCategoria(@RequestBody Categoria categoria) {
+	@PutMapping
+	public ResponseEntity<Categoria> Put(@Valid @RequestBody Categoria categoria) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> AddCategoria(@Valid @RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 	}
 	
 	@DeleteMapping("/{id}")
--- AtividadesSpring/02_06/farmaciaDaMarcia/src/main/java/com/farmaciadamarcia/farmaciaDaMarcia/controller/ProdutoController.java
+++ AtividadesSpring/02_06/farmaciaDaMarcia/src/main/java/com/farmaciadamarcia/farmaciaDaMarcia/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package com.farmaciadamarcia.farmaciaDaMarcia.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +45,12 @@ public class ProdutoController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Produto> AddProduto(@RequestBody Produto produto) {
+	public ResponseEntity<Produto> AddProduto(@Valid @RequestBody Produto produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> Put(@Valid @RequestBody Produto produto) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
 	}
 	
